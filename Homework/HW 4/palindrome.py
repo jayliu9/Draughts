c1b0f377e3c00dc132c84061a24900a338daccd0
liushijie <liu.shijie@northeastern.edu>
2020-10-12 18:37:49 +0800

    HW4 problem 1 finished

--- Homework/HW 4/palindrome.py
+++ Homework/HW 4/palindrome.py
@@ -1,74 +1,6 @@
 '''
 Name: Shijie Liu
 NUID: 001561546
-Course Number: 18533
-Semester: Fall 2020
-
-This code is for performing basic arithmetic.
-'''
-
-
-def calculate_subtotal(num1, operator, num2):
-    '''
-        Function - calculate_subtotal
-            Calculates the subtotal of two numbers
-        Parameters:
-            num1 -- The number before the operator
-            operator -- A arithmetic operator. Assumes the operator is among
-            "+", "-", "*" and "/"
-            num2 -- The number after the operator
-        Returns:
-            The subtotal after performing the requested operation.
-    '''
-    if operator == "+":
-        subtotal = num1 + num2
-    elif operator == "-":
-        subtotal = num1 - num2
-    elif operator == "*":
-        subtotal = num1 * num2
-    elif operator == "/":
-        subtotal = num1 / num2
-    return subtotal
-
-
-def judgement_of_quitting(user_in):
-    '''
-        Function - judgement_of_quitting
-            Determines whether the loop is requested to quit
-        Parameters:
-            user_in -- The user's input. Assumes the input is "Q", "q", or
-            inputs of basic arithmetic in specified format  
-        Returns:
-            The boolean value of whether to quit the loop
-    '''
-    QUIT_FLAG = "Q"
-    
-    should_quit = user_in.upper() == QUIT_FLAG
-    if should_quit:
-        return False
-    return True
-
-
-def main():
-    PROMPT = "Enter the next step in the calculation: "
-
-    initial_value = float(input("Enter a number: "))
-    step2 = input(PROMPT)
-    while judgement_of_quitting(step2):
-        operator, num_str = step2.split()
-        value = float(num_str)
-        subtotal = calculate_subtotal(initial_value, operator, value)
-        print("Subtotal =", subtotal)
-        initial_value = subtotal
-        step2 = input(PROMPT)
-    print("Total =", subtotal)
-
-
-if __name__ == "__main__":
-    main()
-'''
-Name: Shijie Liu
-NUID: 001561546
 Course: CS 5001
 Course Number: 18529
 Semester: Fall 2020
@@ -77,4 +9,32 @@ The code is for determining whether the string is a palindrome.
 '''
 
 
-def is_palindrome():
+def is_palindrome(raw_str):
+    '''
+        Function - is_palindrome
+            Determines whether the string is a palindrome, which is any string
+            that reads the same backwards and forwards, ignoring letter case
+            and spaces and is at least 2 characters in length.
+        Parameters:
+            raw_str -- A string that can include non-letters, such as
+            punctuation and numbers.
+        Returns:
+            The boolean value of whether the string is a palindrome
+    '''
+    normalized_str = raw_str.replace(" ","").lower()
+    is_a_palindrome = (len(normalized_str) >= 2 and
+                       normalized_str[::-1] == normalized_str)
+    if is_a_palindrome:
+        return True
+    return False
+
+
+def main():
+    print(is_palindrome("madam  Im adam"))
+    print(is_palindrome("a"))
+    print(is_palindrome("RAdar"))
+    print(is_palindrome("!radar!"))
+
+
+if __name__ == "__main__":
+    main()
