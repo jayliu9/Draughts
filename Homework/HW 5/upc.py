f5ffd81caecdee214b9b67abaec800e233960982
liushijie <liu.shijie@northeastern.edu>
2020-10-19 17:28:24 +0800

    HW 5 not finished

--- Homework/HW 5/upc.py
+++ Homework/HW 5/upc.py
@@ -5,27 +5,35 @@ Course: CS 5001
 Course Number: 18529
 Semester: Fall 2020
 
-The code is a function that takes a string as input and determines whether or
-not the supplied string is a valid UPC.
+The code is a function that takes a string as input and tells whether or not
+the supplied string is a valid UPC.
 '''
 
 
-def is_valid_upc(upc):
+def is_valid_upc(user_in):
+    '''
+        Function -- is_valid_upc
+            Checks whether of not the supplied string is a valid UPC
+        Parameter:
+            user_in -- A string to consider.
+        Returns:
+            True if the supplied string is a valid UPC, False otherwise
+    '''
     ODD_POSITION_COEFFCIENT = 3
     DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     FACTOR = 10
 
     sum = 0
-    len_of_upc = len(upc)
-    if len(upc) == 0:
+    user_in_len = len(user_in)
+    if len(user_in) == 0:
         return False
-    for i in range(len_of_upc - 1, -1, -1):
-        if upc[i] not in DIGITS:
+    for i in range(user_in_len - 1, -1, -1):
+        if user_in[i] not in DIGITS:
             return False
-        current_position = (len_of_upc - 1) - i
+        current_position = (user_in_len - 1) - i
         if current_position % 2 == 0:
-            sum += int(upc[i])
+            sum += int(user_in[i])
         else:
-            sum += int(upc[i]) * ODD_POSITION_COEFFCIENT
+            sum += int(user_in[i]) * ODD_POSITION_COEFFCIENT
     is_valid = sum % FACTOR == 0
     return is_valid
