1fad9e002782f4e756fe3c6df6ccfd7e42285a6a
liushijie <liu.shijie@northeastern.edu>
2020-12-07 00:42:18 +0800

    lab 11 finished

--- Labs/Lab 11/min_in_shifted_list.py
+++ Labs/Lab 11/min_in_shifted_list.py
@@ -4,20 +4,28 @@ NUID: 001561546
 Course Number: 18533
 Semester: Fall 2020
 
-This code is a function that finds the minimum in a shifted list.
+This code is a function that searches for the smallest integer in a
+sorted list of integers which are shifted some unknown number of times.
 '''
 
 
-def find_min(shifted_lst):
+def search_for_min(shifted_lst):
+    '''
+        Function -- searches_for_min
+            Searches for the smallest integer in a sorted list of
+            integers which are shifted some unknown number of times.
+        Parameters:
+            shifted_lst -- The sorted-shifted list of integers to search in.
+        Returns:
+            The smallest integer in the sorted-shifted list.
+    '''
+    if shifted_lst[0] <= shifted_lst[-1]:
+        return shifted_lst[0]
     midpoint = len(shifted_lst) // 2
     left_lst = shifted_lst[:midpoint]
     right_lst = shifted_lst[midpoint:]
-
-    if left_lst[0] <= left_lst[-1] and right_lst[0] <= right_lst[-1]:
-        if shifted_lst[0] < shifted_lst[-1]:
-            return shifted_lst[0]
-        return shifted_lst[1]
-    elif left_lst[0] > left_lst[-1]:
-        return find_min(left_lst)
-    else:
-        return find_min(right_lst)
+    if shifted_lst[midpoint] > shifted_lst[-1]:
+        return search_for_min(right_lst)
+    elif shifted_lst[midpoint] < shifted_lst[midpoint - 1]:
+        return shifted_lst[midpoint]
+    return search_for_min(left_lst)
