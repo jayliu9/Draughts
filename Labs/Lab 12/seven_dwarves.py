7307d0fcb2f7875b343b8ac80de98dd480a3b1e7
liushijie <liu.shijie@northeastern.edu>
2020-12-13 23:42:27 +0800

    Lab 12 finished

--- Labs/Lab 12/seven_dwarves.py
+++ Labs/Lab 12/seven_dwarves.py
@@ -57,11 +57,30 @@ def line_processing(line):
 
 
 def generate_line(key, dic):
+    '''
+        Function -- generate_line
+            Generates a string as a line of the text file from a dictionary
+            item based on the original format.
+        Parameters:
+            key -- The key of the dictionary item
+            dic -- The dictionary
+        Returns:
+            A string as a line of the text file in required format
+    '''
     WHITESPACE = " "
     return key + WHITESPACE + WHITESPACE.join(dic[key])
 
 
 def generate_text(network_dic):
+    '''
+        Function -- generate_line
+            Generates a list of the whole network in the original format from
+            the dictionary of the network.
+        Parameters:
+            network_dic -- The dictionary of the network.
+        Returns:
+            The list of the whole network in the original format.
+    '''
     text_lines = []
     for key in network_dic:
         line = generate_line(key, network_dic)
@@ -70,27 +89,68 @@ def generate_text(network_dic):
 
 
 def write_file(path, content):
-        with open(path, "w") as file:
-            for line in content:
-                file.write(line + "\n")
+    '''
+        Function -- write_file
+            Writes a text into a file.
+        Parameters:
+            path -- A string file path
+            content -- A list of content to write
+        Returns:
+            None. This function does not return.
+    '''
+    with open(path, "w") as file:
+        for line in content:
+            file.write(line + "\n")
 
-     
-def unfriending(subject_name, object_name, network):
-    network[subject_name].remove(object_name)
+
+def unfriending(subject_dwarf, object_dwarf, network):
+    '''
+        Function -- unfriending
+            Unfriends someone.
+        Parameters:
+            subject_dwarf -- The dwarf who unfriends someone.
+            object_dwarf -- The dwarf to unfriend
+            network -- The dictionary of the whole network
+        Returns:
+            Nothing.
+    '''
+    network[subject_dwarf].remove(object_dwarf)
 
 
 def is_empty_list(lst):
+    '''
+        Function -- is_empty_list
+            Checks if the given list is an empty string.
+        Parameter:
+            lst -- The list to check
+        Returns:
+            True if the list is empty, False otherwise.
+    '''
     return len(lst) == 0
 
 
-def friending_validator(name, friend_lst):
+def friending_validator(dwarf_to_friend, friend_lst):
+    '''
+        Function -- friending_validator
+            Checks that a dwarf to friend exists but is not included in the
+            friend list.
+        Parameter:
+            dwarf_to_friend -- The dwarf to friend
+            friend_lst -- The friend list of the dwarf who friends someone
+        Raises:
+            KeyError -- If the dwarf to friend does not exist.
+            ValueError -- If the dwarf to friend have been included in the
+            friend list of the dwarf who is logging in.
+        Returns:
+            Nothing
+    '''
     SEVEN_DWARVES = ("Happy", "Dopey", "Bashful", "Sneezy", "Sleepy", "Doc",
                      "Grumpy")
 
-    if name in friend_lst:
-        raise ValueError
-    if name not in SEVEN_DWARVES:
+    if dwarf_to_friend not in SEVEN_DWARVES:
         raise KeyError
+    if dwarf_to_friend in friend_lst:
+        raise ValueError
 
 
 def main():
@@ -102,7 +162,7 @@ def main():
     path = "dwarves.txt"
     file_list = read_file(path)
     network = create_network(file_list)
-    dwarf_name = input("Which of the 7 dwarves is logging in? ")
+    logging_in_dwarf = input("Which of the 7 dwarves is logging in? ")
     should_quit = False
     while not should_quit:
         print("Choose from one of the options below:")
@@ -115,29 +175,29 @@ def main():
         user_choice = user_in.split()[0]
 
         if user_choice == PRINT_FRIENDS:
-            print("Your friends:", ", ".join(network[dwarf_name]))
+            print("Your friends:", ", ".join(network[logging_in_dwarf]))
         elif user_choice == UNFRIDEND:
-            chosen_name = user_in.split()[1]
-            if is_empty_list(network[dwarf_name]):
+            chosen_dwarf = user_in.split()[1]
+            if is_empty_list(network[logging_in_dwarf]):
                 print("You don't have any friends.")
             else:
                 try:
-                    unfriending(dwarf_name, chosen_name, network)
-                    unfriending(chosen_name, dwarf_name, network)
-                    print("{} has been unfriended".format(chosen_name))
+                    unfriending(logging_in_dwarf, chosen_dwarf, network)
+                    unfriending(chosen_dwarf, logging_in_dwarf, network)
+                    print("{} has been unfriended".format(chosen_dwarf))
                 except ValueError:
-                    print("{} is not your friend".format(chosen_name))
+                    print("{} is not your friend".format(chosen_dwarf))
         elif user_choice == FRIEND:
-            chosen_name = user_in.split()[1]
+            chosen_dwarf = user_in.split()[1]
             try:
-                friending_validator(chosen_name, network[dwarf_name])
-                network[dwarf_name].append(chosen_name)
-                network[chosen_name].append(dwarf_name)
-                print("{} has been friended".format(chosen_name))
+                friending_validator(chosen_dwarf, network[logging_in_dwarf])
+                network[logging_in_dwarf].append(chosen_dwarf)
+                network[chosen_dwarf].append(logging_in_dwarf)
+                print("{} has been friended".format(chosen_dwarf))
             except ValueError:
-                print("{} was your friend.".format(chosen_name))
+                print("{} was your friend.".format(chosen_dwarf))
             except KeyError:
-                print("{} doesn't exist.".format(chosen_name))
+                print("{} doesn't exist.".format(chosen_dwarf))
         elif user_choice == QUIT:
             should_quit = True
         else:
